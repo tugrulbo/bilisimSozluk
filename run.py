52dedaa7efa6006bca3b9b792e8de6f67023c76b
tugrulbo <tugrulbo@gmail.com>
2020-08-11 01:01:13 +0300

    Güncellemeler
    Tamamıyla düzenlendi ve bitirildi

--- run.py
+++ run.py
@@ -6,9 +6,9 @@ import configparser
 import json
 
 ##Excel Dosyası İşlemleri##
-data = pd.read_excel ("../bilisimsozlukbotu/words.xlsx")
-df_English = pd.DataFrame(data, columns= ['English']) #Excel dosyasındaki belirli bir sutunu çekme
-df_Turkish = pd.DataFrame(data, columns= ['Turkish']) #Excel dosyasındaki belirli bir sutunu çekme
+data = pd.read_excel ("../bilisimsozluk/newList.xlsx")#Excel dosyasının bulunduğu dosya yolu
+df_English = pd.DataFrame(data, columns= ['english']) #Excel dosyasındaki belirli bir sutunu çekme
+df_Turkish = pd.DataFrame(data, columns= ['turkish']) #Excel dosyasındaki belirli bir sutunu çekme
 english_Words = df_English.values.tolist()#Excel dosyasındaki bilgileri listeye çevirme
 turkish_Words = df_Turkish.values.tolist()#Excel dosyasındaki bilgileri listeye çevirme
 
@@ -16,6 +16,8 @@ turkish_Words = df_Turkish.values.tolist()#Excel dosyasındaki bilgileri listeye
 for i in range(10):
     print(english_Words[i])
     print(turkish_Words[i])
+
+
 
 #Veritabanı Bağlantısı
 mydb = mysql.connector.connect(
@@ -26,14 +28,34 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+k = 0
+if k<=len(english_Words):
+  for k in range(len(english_Words)):
+    word = english_Words[k]
+    strWord = json.dumps(word)
+    startWord = strWord.replace('"',"")
+    en = startWord.replace("[","")
+    enWord = en.replace("]","")
+    
+    wordT = turkish_Words[k]
+    strWordT = json.dumps(wordT,ensure_ascii=False)
+    startWordT = strWordT.replace('"',"")
+    tr = startWordT.replace("[","")
+    trWord = tr.replace("]","")
+    if(enWord =="NaN"):
+      k += 1
+      print(f"NaN:{k}")
+      continue
+    sql = "INSERT INTO words (english, turkish) VALUES (%s, %s)"
+    val = (enWord, trWord)
+    mycursor.execute(sql, val)
+    mydb.commit()
+else:
+  print("Bir hatayla bir karşılaşıldı")   
 
-sql = "INSERT INTO words (english, turkish) VALUES (%s, %s)"
-val = (json.dumps(english_Words[0]), json.dumps(turkish_Words[0]))
-mycursor.execute(sql, val)
 
-mydb.commit()
+print("İşlem tamamlandı")
 
-print(mycursor.rowcount, "record inserted.")
 
 
 
